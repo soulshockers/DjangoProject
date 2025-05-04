26b6b15ad09d2f7041eaba359e97a3762c79b860
Vasyl Sovyak <vasyl.sovyak@paysera.net>
2025-05-05 00:40:28 +0300

    Fix for create a superuser during the deployment

--- build.sh
+++ build.sh
@@ -6,18 +6,21 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
 
 # Conditionally create superuser using environment variables
 echo "Checking if superuser exists..."
-SUPERUSER_EXISTS=$(python manage.py shell -c "
+SUPERUSER_EXISTS=$(python manage.py shell -v 0 -c "
 from django.contrib.auth import get_user_model;
 User = get_user_model();
-print(User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists())
+print(User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists() and 'True' or 'False', end='')
 ")
+
+# Print the value of SUPERUSER_EXISTS for debugging
+echo "SUPERUSER_EXISTS: $SUPERUSER_EXISTS"
 
 if [ "$SUPERUSER_EXISTS" = "False" ]; then
   echo "Superuser does not exist, creating..."
